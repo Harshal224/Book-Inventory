5721a9c94e22328c572656ef20f0cd87f713998c
Harshal224 <harshalbankar2222@gmail.com>
2026-01-23 10:11:45 +0530

    Added mock API using JSON Server and API service layer

--- book-inventory/src/components/Home.jsx
+++ book-inventory/src/components/Home.jsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+import { getBooks } from "../services/bookService";
+
 function Home() {
+  useEffect(() => {
+    getBooks().then(res => console.log(res.data));
+  }, []);
+
   return (
     <div className="container mt-4">
       <h4>Book Inventory</h4>
