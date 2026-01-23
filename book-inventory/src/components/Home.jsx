bef64a5050276c63784e9aa3f25eee97c3043d48
Harshal224 <harshalbankar2222@gmail.com>
2026-01-23 20:43:22 +0530

    Displayed books in table with API integration and scrolling

--- book-inventory/src/components/Home.jsx
+++ book-inventory/src/components/Home.jsx
@@ -1,15 +1,45 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getBooks } from "../services/bookService";
 
 function Home() {
+  const [books, setBooks] = useState([]);
+
   useEffect(() => {
-    getBooks().then(res => console.log(res.data));
+    getBooks().then(res => setBooks(res.data));
   }, []);
 
   return (
     <div className="container mt-4">
       <h4>Book Inventory</h4>
-      <p className="text-muted">Manage your book collection</p>
+      <p className="text-muted">
+        Manage your book collection
+      </p>
+
+      <div className="card mt-3">
+        <div
+          className="card-body table-responsive"
+          style={{ maxHeight: "400px" }}
+        >
+          <table className="table table-striped table-hover">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Publisher</th>
+              </tr>
+            </thead>
+            <tbody>
+              {books.map(book => (
+                <tr key={book.id}>
+                  <td>{book.title}</td>
+                  <td>{book.author}</td>
+                  <td>{book.publisher}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
